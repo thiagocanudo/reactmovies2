33bd56d9ef994515866d342094defc96e0ae0450
thiago canudo <thiago@thiagocanudo.com.br>
2024-11-16 14:39:02 -0300

    Modal Trailler

--- src/api/tmdb.js
+++ src/api/tmdb.js
@@ -1,53 +1,44 @@
-import axios from 'axios';
-
-const API_KEY = '81154aa94ab4961e8bc2a66dad78bf47';
-const BASE_URL = 'https://api.themoviedb.org/3';
+import axios from "axios";
+const API_KEY = "81154aa94ab4961e8bc2a66dad78bf47";
+const BASE_URL = "https://api.themoviedb.org/3";
 
 // https://api.themoviedb.org/3/movie/popular?api_key=81154aa94ab4961e8bc2a66dad78bf47
 // https://api.themoviedb.org/3/tv/popular?api_key=81154aa94ab4961e8bc2a66dad78bf47
 
-// não existe uma função 'awayt' sem que esteja dentro de uma função 'async'
 // Função que irá buscar os itens (filmes e series)
 export async function getData(categoria, page, ordem) {
-  const endpoint = categoria == 'filmes' ? 'movie' : 'tv';
-
+  // A função 'awayt' precisa estar dentro de uma função 'async'
+  const endpoint = categoria == "filmes" ? "movie" : "tv";
   const response = await axios.get(`${BASE_URL}/${endpoint}/${ordem}`, {
     params: {
       api_key: API_KEY,
-      language: 'PT-BR',
+      language: "pt-BR",
       page: page,
     },
   });
-
   return response.data.results;
 }
 
 export async function getDataVideos(categoria, id) {
-  const endpoint = categoria == 'filmes' ? 'movie' : 'tv';
-
-  // const response = await axios.get(`${BASE_URL}/${endpoint}/${id}/videos`, {
-  const response = await axios.get(
-    `${BASE_URL}/${endpoint}/1396/season/1/episode/1/videos`,
-    {
-      params: {
-        api_key: API_KEY,
-        language: 'PT-BR',
-      },
+  const endpoint = categoria == "filmes" ? "movie" : "tv";
+  const response = await axios.get(`${BASE_URL}/${endpoint}/${id}/videos`, {
+    //`${BASE_URL}/${endpoint}/1396/season/1/episode/1/videos`,
+    params: {
+      api_key: API_KEY,
+      language: "pt-BR",
     },
-  );
+  });
   return response.data.results;
 }
 
-// Função que irá buscar o filme pelo ID
+// Função que busca o filme pelo ID
 export async function getDataId(categoria, id) {
-  const endpoint = categoria == 'filmes' ? 'movie' : 'tv';
-
+  const endpoint = categoria == "filmes" ? "movie" : "tv";
   const response = await axios.get(`${BASE_URL}/${endpoint}/${id}`, {
     params: {
       api_key: API_KEY,
-      language: 'PT-BR',
+      language: "pt-BR",
     },
   });
-
   return response.data;
 }
--- src/pages/Detalhes.jsx
+++ src/pages/Detalhes.jsx
@@ -1,21 +1,23 @@
-import { useEffect, useState } from 'react';
-import { useNavigate, useParams } from 'react-router-dom';
-import { getDataId, getDataVideos } from '../api/tmdb';
-import { BarLoader } from 'react-spinners';
+import { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { getDataId, getDataVideos } from "../api/tmdb";
+import { BarLoader } from "react-spinners";
 
 export function Detalhes() {
   // console.log(useParams());
   // console.log("desestruturação do objeto: " +useParams().id+ " e " +useParams().categoria);
 
-  const [loading, setLoading] = useState(true);
-
   const { categoria } = useParams();
   const { id } = useParams();
-
   const navigate = useNavigate();
 
+  const [loading, setLoading] = useState(true);
   const [item, setItem] = useState([]);
   const [itemVideo, setItemVideo] = useState([]);
+
+  const [showModal, setShowModal] = useState(false);
+
+  const urlVideo = itemVideo[itemVideo.length - 1];
 
   async function loadData() {
     setLoading(true);
@@ -23,7 +25,7 @@ export function Detalhes() {
       const data = await getDataId(categoria, id);
       setItem(data);
     } catch (error) {
-      console.log('Erro ao buscar dados: ', error);
+      console.log("Erro ao buscar dados: ", error);
     }
   }
 
@@ -36,7 +38,7 @@ export function Detalhes() {
         setLoading(false);
       }, 1000);
     } catch (error) {
-      console.log('Erro ao buscar dados: ', error);
+      console.log("Erro ao buscar dados: ", error);
     }
   }
 
@@ -46,7 +48,7 @@ export function Detalhes() {
   }, []);
 
   if (loading) {
-    return <BarLoader width={'100%'} color="#00B1E9" className="mt-24" />;
+    return <BarLoader width={"100%"} color="#00B1E9" className="mt-24" />;
   }
 
   return (
@@ -59,20 +61,15 @@ export function Detalhes() {
           alt=""
         />
       </div>
-
       <div
         className="
                 flex
                 w-[90%]
                 max-w-[850px]
-                mt-3
+                mt-6
+                mb-20
                 mx-auto
-
-                lg:absolute
-                lg:top-40
-                lg:left-[50%]
-                lg:ml-[-425px]
-
+                relative 
                 bg-brand-dark
                 bg-opacity-50 
                 backdrop-blur-sm
@@ -92,32 +89,26 @@ export function Detalhes() {
           <h2 className="mt-2 font-bold text-lg">{item.title || item.name}</h2>
           <ul>
             <li>
-              Ano:{' '}
+              Ano:{" "}
               {item.first_air_date?.substring(0, 4) ||
                 item.release_date?.substring(0, 4)}
             </li>
             <li>Avaliação: {item.vote_average?.toFixed(1)}</li>
+            <li>{item.title || item.name}</li>
           </ul>
 
-          <div className="bg-black">
-            {/* {categoria === 'filmes' && ()} */}
-            {id}
-            <iframe
-              className="mx-auto"
-              width="560"
-              height="315"
-              src={`https://www.youtube.com/embed/${
-                itemVideo[itemVideo.length - 1].key
-              }?si=EUyYitu5wVAnXqz5`}
-              title="YouTube video player"
-              frameborder="0"
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-              referrerpolicy="strict-origin-when-cross-origin"
-              allowfullscreen
-            ></iframe>
-          </div>
+          {/* <div className="bg-black"></div> */}
 
           <p className="mt-2">{item.overview}</p>
+
+          {urlVideo && (
+            <button
+              onClick={() => setShowModal(true)}
+              className="bg-brand-blue-light hover:bg-brand-yellow text-gray-50 hover:text-brand-dark mr-3 py-2 px-10 font-bold rounded my-5"
+            >
+              Trailler
+            </button>
+          )}
 
           <button
             onClick={() => navigate(-1)}
@@ -127,6 +118,58 @@ export function Detalhes() {
           </button>
         </div>
       </div>
+
+      {showModal ? (
+        <>
+          <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+            <div className="relative w-auto my-6 mx-auto max-w-3xl">
+              {/*content*/}
+              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                {/*header*/}
+                <div className="flex items-start justify-between p-5 border-b border-solid border-blueGray-200 rounded-t">
+                  <h3 className="text-3xl font-semibold text-brand-blue-dark">
+                    {item.title || item.name}
+                  </h3>
+                  <button
+                    className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                    onClick={() => setShowModal(false)}
+                  >
+                    <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
+                      ×
+                    </span>
+                  </button>
+                </div>
+                {/*body*/}
+                <div className="relative p-6 flex-auto">
+                  {urlVideo && (
+                    <iframe
+                      className="mx-auto"
+                      width="560"
+                      height="315"
+                      src={`https://www.youtube.com/embed/${urlVideo.key}?si=EUyYitu5wVAnXqz5`}
+                      title="YouTube video player"
+                      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                      referrerPolicy="strict-origin-when-cross-origin"
+                      allowFullScreen
+                    ></iframe>
+                  )}
+                </div>
+                {/*footer*/}
+                <div className="flex items-center justify-end p-6 border-t border-solid border-blueGray-200 rounded-b">
+                  <button
+                    className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                    type="button"
+                    onClick={() => setShowModal(false)}
+                  >
+                    Close
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+        </>
+      ) : null}
     </>
   );
 }
